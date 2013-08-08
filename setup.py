ae3f1e23eb19658d6b981daec0c31625c4fb6827
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-08-08 19:33:35 +0800

    Add version information to package.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = 'wrapt',
-      version = '0.9',
+      version = '0.9.0',
       description = 'Module for decorators, wrappers and monkey patching.',
       author = 'Graham Dumpleton',
       author_email = 'Graham.Dumpleton@gmail.com',
--- wrapt/__init__.py
+++ wrapt/__init__.py
@@ -1,3 +1,6 @@
+__version_info__ = ('0', '9', '0')
+__version__ = '.'.join(__version_info__)
+
 from .wrappers import FunctionWrapper
 from .decorators import decorator, adapter
 from .exceptions import (UnexpectedDefaultParameters, MissingDefaultParameter,
