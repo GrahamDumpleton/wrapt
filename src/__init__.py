ed5716f542bdac8f40db8f8d7f1d3bb987873a7e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-10-15 22:06:09 +1100

    Revert version back to 1.2.1 to allow for a patch release.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '3', '0')
+__version_info__ = ('1', '2', '1')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, FunctionWrapper, BoundFunctionWrapper,
