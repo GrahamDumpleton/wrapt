b6e47be49ceef31db966a93046371a57114f0eaa
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-12-19 21:37:26 +1100

    Export transient_function_wrapper via top level wrapt module.

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,8 @@ __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, FunctionWrapper, BoundFunctionWrapper,
         WeakFunctionProxy, resolve_path, apply_patch, wrap_object,
-        function_wrapper, wrap_function_wrapper, patch_function_wrapper)
+        function_wrapper, wrap_function_wrapper, patch_function_wrapper,
+        transient_function_wrapper)
 
 from .decorators import decorator, synchronized
 
