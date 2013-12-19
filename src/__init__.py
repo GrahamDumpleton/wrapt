1dbda310ac2375550952fc53c940e45c14f5f792
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-12-19 22:12:02 +1100

    Increment version to 1.3.1.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '3', '0')
+__version_info__ = ('1', '3', '1')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, FunctionWrapper, BoundFunctionWrapper,
