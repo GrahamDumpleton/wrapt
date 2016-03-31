9e7787cc3eda1d03da1d8660a66a3baba9ffbeab
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-03-31 16:36:09 +1100

    Increment version to 1.10.7.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '10', '6')
+__version_info__ = ('1', '10', '7')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, CallableObjectProxy, FunctionWrapper,
