17641311acb62b57219312597e71bfa0c033fad9
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-03-15 11:22:59 +1100

    Increment version to 1.10.10.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class optional_build_ext(build_ext):
 
 setup_kwargs = dict(
       name='wrapt',
-      version='1.10.9',
+      version='1.10.10',
       description='Module for decorators, wrappers and monkey patching.',
       long_description=open('README.rst').read(),
       author='Graham Dumpleton',
