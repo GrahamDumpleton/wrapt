7f69650d054da8ab0891047af55f93ca936aa773
Hugo <hugovk@users.noreply.github.com>
2018-08-05 13:12:39 +0300

    Drop support for EOL Python 2.6 and 3.3

--- setup.py
+++ setup.py
@@ -35,12 +35,13 @@ class optional_build_ext(build_ext):
 classifiers = [
     'Development Status :: 5 - Production/Stable',
     'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
 ]
