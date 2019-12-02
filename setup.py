4db7f027030f66bf183113ca7b662ce0dd5ae488
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-12-02 11:13:53 +1100

    Increment version to 1.12.0.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ classifiers = [
 
 setup_kwargs = dict(
       name='wrapt',
-      version='1.11.2',
+      version='1.12.0',
       description='Module for decorators, wrappers and monkey patching.',
       long_description=open('README.rst').read(),
       author='Graham Dumpleton',
