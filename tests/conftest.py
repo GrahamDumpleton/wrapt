e94bb82688278186301dc98447596ac7f67808c9
Varun Agrawal <varagrawal@gmail.com>
2021-05-05 19:00:56 -0400

    update pytest.File to new scheme

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,16 @@ import pytest
 
 version = tuple(sys.version_info[:2])
 
-class DummyCollector(pytest.collect.File):
+class DummyCollector(pytest.File):
     def collect(self):
         return []
 
 def pytest_pycollect_makemodule(path, parent):
     if '_py33' in path.basename and version < (3, 3):
-        return DummyCollector(path, parent=parent)
+        return DummyCollector.from_parent(parent, fspath=path)
     if '_py37' in path.basename and version < (3, 7):
-        return DummyCollector(path, parent=parent)
+        return DummyCollector.from_parent(parent, fspath=path)
     if '_py3' in path.basename and version < (3, 0):
-        return DummyCollector(path, parent=parent)
+        return DummyCollector.from_parent(parent, fspath=path)
     if '_py2' in path.basename and version >= (3, 0):
-        return DummyCollector(path, parent=parent)
+        return DummyCollector.from_parent(parent, fspath=path)
