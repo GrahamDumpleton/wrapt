458597ec6977056350bf266cbb80b4f5f294c10f
Varun Agrawal <varagrawal@gmail.com>
2021-05-05 20:28:23 -0400

    configure conftest to work with python 2 and 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,15 @@ class DummyCollector(pytest.File):
         return []
 
 def pytest_pycollect_makemodule(path, parent):
-    if '_py33' in path.basename and version < (3, 3):
-        return DummyCollector.from_parent(parent, fspath=path)
-    if '_py37' in path.basename and version < (3, 7):
-        return DummyCollector.from_parent(parent, fspath=path)
-    if '_py3' in path.basename and version < (3, 0):
-        return DummyCollector.from_parent(parent, fspath=path)
-    if '_py2' in path.basename and version >= (3, 0):
-        return DummyCollector.from_parent(parent, fspath=path)
+    if version < (3, 0):
+        if '_py27' in path.basename:
+            return DummyCollector(path, parent=parent)
+    else:
+        if '_py33' in path.basename and version < (3, 3):
+            return DummyCollector.from_parent(parent, fspath=path)
+        if '_py37' in path.basename and version < (3, 7):
+            return DummyCollector.from_parent(parent, fspath=path)
+        if '_py3' in path.basename and version < (3, 0):
+            return DummyCollector.from_parent(parent, fspath=path)
+        if '_py2' in path.basename and version >= (3, 0):
+            return DummyCollector.from_parent(parent, fspath=path)
