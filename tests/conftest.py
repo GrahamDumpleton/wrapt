d256153e76e94d4fa98cca66234c1f22002500a0
Varun Agrawal <varagrawal@gmail.com>
2021-05-05 20:51:28 -0400

    correct use of DummyCollector

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,20 +4,27 @@ import pytest
 
 version = tuple(sys.version_info[:2])
 
+
 class DummyCollector(pytest.File):
     def collect(self):
         return []
 
-def pytest_pycollect_makemodule(path, parent):
-    if version < (3, 0):
-        if '_py27' in path.basename:
-            return DummyCollector(path, parent=parent)
+
+def collect(path, parent):
+    """Helper function deal with differences between Python 2 and 3."""
+    if version[0] == 2:
+        return DummyCollector(path, parent=parent)
     else:
-        if '_py33' in path.basename and version < (3, 3):
-            return DummyCollector.from_parent(parent, fspath=path)
-        if '_py37' in path.basename and version < (3, 7):
-            return DummyCollector.from_parent(parent, fspath=path)
-        if '_py3' in path.basename and version < (3, 0):
-            return DummyCollector.from_parent(parent, fspath=path)
-        if '_py2' in path.basename and version >= (3, 0):
-            return DummyCollector.from_parent(parent, fspath=path)
+        return DummyCollector.from_parent(parent, fspath=path)
+
+
+def pytest_pycollect_makemodule(path, parent):
+    """Use a dummy collector to collect version-incompatible test files."""
+    if '_py33' in path.basename and version < (3, 3):
+        return collect(path, parent)
+    if '_py37' in path.basename and version < (3, 7):
+        return collect(path, parent)
+    if '_py3' in path.basename and version < (3, 0):
+        return collect(path, parent)
+    if '_py2' in path.basename and version >= (3, 0):
+        return collect(path, parent)
