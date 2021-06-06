8cb7693fa5ebdbb27904b75c9b930e65a7104f37
Martin Larralde <martin.larralde@embl.de>
2021-06-06 14:59:25 +0200

    Rewrite `setup.py` with a complementary `setup.cfg` metadata file

--- setup.py
+++ setup.py
@@ -1,122 +1,33 @@
-from __future__ import print_function
-
 import os
+import setuptools
 import sys
 
-from distutils.core import setup
-from distutils.core import Extension
-from distutils.command.build_ext import build_ext
-from distutils.errors import (CCompilerError, DistutilsExecError,
-                DistutilsPlatformError)
 
-if sys.platform == 'win32':
-    build_ext_errors = (CCompilerError, DistutilsExecError,
-            DistutilsPlatformError, IOError, OSError, ValueError)
+# # --- Detect if extensions should be disabled ------------------------------
+
+wrapt_env = os.environ.get('WRAPT_INSTALL_EXTENSIONS')
+if wrapt_env is None:
+    wrapt_env = os.environ.get('WRAPT_EXTENSIONS')
+if wrapt_env is not None:
+    disable_extensions = wrapt_env.lower() == 'false'
 else:
-    build_ext_errors = (CCompilerError, DistutilsExecError,
-            DistutilsPlatformError)
+    disable_extensions = False
+if sys.implementation.name != "cpython":
+    disable_extensions = True
 
-class BuildExtFailed(Exception):
-    pass
+# --- C extension ------------------------------------------------------------
 
-class optional_build_ext(build_ext):
-    def run(self):
-        try:
-            build_ext.run(self)
-        except DistutilsPlatformError:
-            raise BuildExtFailed()
-
-    def build_extension(self, ext):
-        try:
-            build_ext.build_extension(self, ext)
-        except build_ext_errors:
-            raise BuildExtFailed()
-
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Programming Language :: Python :: Implementation :: PyPy',
+extensions = [
+    setuptools.Extension(
+        "wrapt._wrappers",
+        sources=[ os.path.realpath(os.path.join(__file__, "..", "src", "wrapt", "_wrappers.c"))],
+        optional=True,
+    )
 ]
 
-setup_kwargs = dict(
-      name='wrapt',
-      version='1.12.2',
-      description='Module for decorators, wrappers and monkey patching.',
-      long_description=open('README.rst').read(),
-      author='Graham Dumpleton',
-      author_email='Graham.Dumpleton@gmail.com',
-      license='BSD',
-      classifiers=classifiers,
-      url='https://github.com/GrahamDumpleton/wrapt',
-      packages=['wrapt'],
-      package_dir={'': 'src'},
-     )
 
-def run_setup(with_extensions):
-    setup_kwargs_tmp = dict(setup_kwargs)
+# --- Setup ------------------------------------------------------------------
 
-    if with_extensions:
-        setup_kwargs_tmp['ext_modules'] = [
-                Extension("wrapt._wrappers", ["src/wrapt/_wrappers.c"])]
-        setup_kwargs_tmp['cmdclass'] = dict(build_ext=optional_build_ext)
-
-    setup(**setup_kwargs_tmp)
-
-with_extensions = os.environ.get('WRAPT_INSTALL_EXTENSIONS')
-
-# Use WRAPT_INSTALL_EXTENSIONS now, but also check WRAPT_EXTENSIONS
-# for backward compatibility in case people had hard wired that.
-
-if with_extensions is None:
-    with_extensions = os.environ.get('WRAPT_EXTENSIONS')
-
-if with_extensions:
-    if with_extensions.lower() == 'true':
-        with_extensions = True
-    elif with_extensions.lower() == 'false':
-        with_extensions = False
-    else:
-        with_extensions = None
-
-if hasattr(sys, 'pypy_version_info'):
-    with_extensions = False
-
-WARNING = """
-WARNING: The C extension component for wrapt could not be compiled.
-"""
-
-if with_extensions is not None:
-    run_setup(with_extensions=with_extensions)
-
-else:
-    try:
-        run_setup(with_extensions=True)
-
-    except BuildExtFailed:
-
-        print(75 * '*')
-
-        print(WARNING)
-        print("INFO: Trying to build without extensions.")
-
-        print()
-        print(75 * '*')
-
-        run_setup(with_extensions=False)
-
-        print(75 * '*')
-
-        print(WARNING)
-        print("INFO: Only pure Python version of package installed.")
-
-        print()
-        print(75 * '*')
+setuptools.setup(
+    ext_modules=[] if disable_extensions else extensions
+)
