87c0fa5c71dd1d9c1591a63165a57a403ee55b45
Varun Agrawal <varagrawal@gmail.com>
2021-08-03 19:24:34 -0400

    simplify test

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -225,27 +225,23 @@ class TestDynamicAdapter(unittest.TestCase):
 
     def test_adapter_factory_with_type_hints(self):
         def argspec_factory(wrapped):
-            """argspec factory stub."""
-            argspec = inspect.getfullargspec(wrapped)
-            return argspec
+            return inspect.getfullargspec(wrapped)
 
         def add_option(func):
             """Decorator with custom adapter factory."""
             @wrapt.decorator(adapter=wrapt.adapter_factory(argspec_factory))
             def _add_option(func, instance, args, kwargs):
-                # Call the function.
-                ret = func(*args, **kwargs)
-                return ret
+                return func(*args, **kwargs)
 
             return _add_option(func)
 
         @add_option
         def run() -> Iterable:
             """We add the non-default typehint `Iterable`, which reveals the error."""
-            return []
+            return [True]
 
         # This should run without a NameError being thrown
-        run()
+        self.assertTrue(run())
 
 
 if __name__ == '__main__':
