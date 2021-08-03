267716acae9f518e9cf07671521c4c95238504f7
Varun Agrawal <varagrawal@gmail.com>
2021-08-03 19:19:04 -0400

    add test for adapter_factory with typehints

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 
-import unittest
-import inspect
+import collections
 import imp
+import inspect
+import unittest
+from typing import Iterable
 
 import wrapt
 
@@ -220,6 +222,31 @@ class TestDynamicAdapter(unittest.TestCase):
         argspec = inspect.getargspec(_function_1)
 
         self.assertEqual(argspec.args, ['arg0', 'arg1', 'arg2'])
+
+    def test_adapter_factory_with_type_hints(self):
+        def argspec_factory(wrapped):
+            """argspec factory stub."""
+            argspec = inspect.getfullargspec(wrapped)
+            return argspec
+
+        def add_option(func):
+            """Decorator with custom adapter factory."""
+            @wrapt.decorator(adapter=wrapt.adapter_factory(argspec_factory))
+            def _add_option(func, instance, args, kwargs):
+                # Call the function.
+                ret = func(*args, **kwargs)
+                return ret
+
+            return _add_option(func)
+
+        @add_option
+        def run() -> Iterable:
+            """We add the non-default typehint `Iterable`, which reveals the error."""
+            return []
+
+        # This should run without a NameError being thrown
+        run()
+
 
 if __name__ == '__main__':
     unittest.main()
