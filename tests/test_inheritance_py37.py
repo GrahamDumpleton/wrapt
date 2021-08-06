cd6e17621c163585dd82c21526fb7c626e389f61
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-08-06 16:24:18 +1000

    Add variant of tests using pure Python abc implementation.

--- tests/test_inheritance_py37.py
+++ tests/test_inheritance_py37.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 
 import unittest
+
 import abc
+import _py_abc
 
 import wrapt
 
@@ -66,9 +68,10 @@ class TestClassInheritance(unittest.TestCase):
 
         # XXX The checks commented out below all fail because the override
         # for issubclass() via __subclasscheck__() in abc base class
-        # does not support duck typing and will fail if the argument it
-        # is given is an object proxy like wrapt decorator uses. There
-        # is no known workaround for this problem.
+        # does when C implementation is used does not support duck
+        # typing and will fail if the argument it is given is an object
+        # proxy like wrapt decorator uses. There is no known workaround
+        # for this problem.
         #
         #       def __subclasscheck__(cls, subclass):
         #           """Override for issubclass(subclass, cls)."""
@@ -98,6 +101,54 @@ class TestClassInheritance(unittest.TestCase):
         self.assertTrue(issubclass(A1, (A1, B1, C1, D1)))
         self.assertTrue(issubclass(B1, (A1, B1, C1, D1)))
         # self.assertTrue(issubclass(C1, (A1, B1, C1, D1)))
+        self.assertTrue(issubclass(D1, (A1, B1, C1, D1)))
+
+    def test_py_abc_inheritance(self):
+        @wrapt.decorator
+        def wrapper(wrapped, instance, args, kwargs):
+            return wrapped(args, kwargs)
+
+        class A1(metaclass=_py_abc.ABCMeta):
+            @abc.abstractmethod
+            def method(self):
+                pass
+
+        class B1(A1):
+            def method(self):
+                pass
+
+        @wrapper
+        class C1(B1):
+            def method(self):
+                pass
+
+        class D1(C1):
+            def method(self):
+                pass
+
+        self.assertTrue(issubclass(A1, A1))
+        self.assertTrue(issubclass(B1, A1))
+        self.assertTrue(issubclass(C1, A1))
+        self.assertTrue(issubclass(D1, A1))
+
+        self.assertFalse(issubclass(A1, B1))
+        self.assertTrue(issubclass(B1, B1))
+        self.assertTrue(issubclass(C1, B1))
+        self.assertTrue(issubclass(D1, B1))
+
+        self.assertFalse(issubclass(A1, C1))
+        self.assertFalse(issubclass(B1, C1))
+        self.assertTrue(issubclass(C1, C1))
+        self.assertTrue(issubclass(D1, C1))
+
+        self.assertFalse(issubclass(A1, D1))
+        self.assertFalse(issubclass(B1, D1))
+        self.assertFalse(issubclass(C1, D1))
+        self.assertTrue(issubclass(D1, D1))
+
+        self.assertTrue(issubclass(A1, (A1, B1, C1, D1)))
+        self.assertTrue(issubclass(B1, (A1, B1, C1, D1)))
+        self.assertTrue(issubclass(C1, (A1, B1, C1, D1)))
         self.assertTrue(issubclass(D1, (A1, B1, C1, D1)))
 
 if __name__ == '__main__':
