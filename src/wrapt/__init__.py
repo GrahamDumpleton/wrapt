354f23341d26948092bfedf145a9b8620e5b9af9
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-08-10 08:39:05 +1000

    Increment to 1.13.0rc3.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '13', '0rc2')
+__version_info__ = ('1', '13', '0rc3')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, CallableObjectProxy, FunctionWrapper,
