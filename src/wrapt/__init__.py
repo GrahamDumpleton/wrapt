230e1271364c904678f0975881a790a5ced5c19d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2023-01-11 11:26:44 +1100

    Update version for new changes.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '14', '1')
+__version_info__ = ('1', '15', '0')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, CallableObjectProxy, FunctionWrapper,
