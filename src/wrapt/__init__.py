59da500f4dbdcb1ddbccd96fcf7f372d2320f1ed
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2023-10-07 16:56:00 +1100

    Update version tag to be rc1.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '16', '0dev1')
+__version_info__ = ('1', '16', '0rc1')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import (ObjectProxy, CallableObjectProxy, FunctionWrapper,
