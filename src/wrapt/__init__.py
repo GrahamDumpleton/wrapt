c91f60cca38ed012de06b116acb9339c4a992157
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2024-10-07 10:23:28 +1100

    Explicitly specify name of README file so twine works.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '17', '0dev1')
+__version_info__ = ('1', '17', '0dev2')
 __version__ = '.'.join(__version_info__)
 
 from .__wrapt__ import (ObjectProxy, CallableObjectProxy, FunctionWrapper,
