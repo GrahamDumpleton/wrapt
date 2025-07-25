dd270387507782c51839179abee0003e549e9df1
user <grayjk@gmail.com>
2025-07-25 12:45:06 -0400

    TypeVar instead of ParamSpec

--- src/wrapt/__init__.pyi
+++ src/wrapt/__init__.pyi
@@ -1,13 +1,5 @@
-import sys
-from typing import Any, Callable, TypeVar
+from typing import Callable, TypeVar
 
-R = TypeVar("R")
+F = TypeVar("F", bound=Callable)
 
-if sys.version_info >= (3, 10):
-    from typing import ParamSpec
-
-    P = ParamSpec("P")
-
-    def FunctionWrapper(wrapped: Callable[P, R], wrapper: Callable) -> Callable[P, R]: ...
-else:
-    def FunctionWrapper(wrapped: Callable[..., R], wrapper: Callable) -> Callable[..., R]: ...
+def FunctionWrapper(wrapped: F, wrapper: Callable) -> F: ...
