63acfc2430c146ae0543e6df323e96cca1a81a4b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2025-08-01 12:59:27 +1000

    Eliminate deprecation warnings on pytest conftest file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
-import sys
 import re
+import sys
 
 try:
     from pytest import File as FileCollector
@@ -16,24 +16,24 @@ class DummyCollector(FileCollector):
 
 def construct_dummy(path, parent):
     if hasattr(DummyCollector, "from_parent"):
-        item = DummyCollector.from_parent(parent, fspath=path)
+        item = DummyCollector.from_parent(parent, path=path)
         return item
     else:
         return DummyCollector(path, parent=parent)
 
 
-def pytest_pycollect_makemodule(path, parent):
-    basename = path.basename
+def pytest_pycollect_makemodule(module_path, parent):
+    basename = module_path.name
 
     # Handle Python 2/3 general cases
     if "_py2" in basename and version >= (3, 0):
-        return construct_dummy(path, parent)
+        return construct_dummy(module_path, parent)
     if "_py3" in basename and version < (3, 0):
-        return construct_dummy(path, parent)
+        return construct_dummy(module_path, parent)
 
     # Handle specific Python version cases using regex
     # Match patterns like "_py33", "_py34", "_py310", etc.
-    version_match = re.search(r'_py(\d)(\d*)', basename)
+    version_match = re.search(r"_py(\d)(\d*)", basename)
     if version_match:
         major = int(version_match.group(1))
         minor_str = version_match.group(2)
@@ -41,6 +41,6 @@ def pytest_pycollect_makemodule(path, parent):
 
         # Check if current version is less than the required version
         if version < (major, minor):
-            return construct_dummy(path, parent)
+            return construct_dummy(module_path, parent)
 
     return None
