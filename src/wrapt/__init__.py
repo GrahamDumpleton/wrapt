800a2047f38ce4ad4dc3c821fc4630ea440adc11
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2025-08-12 13:14:29 +1000

    Increment release version.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '17', '2')
+__version_info__ = ('1', '17', '3')
 __version__ = '.'.join(__version_info__)
 
 from .__wrapt__ import (ObjectProxy, CallableObjectProxy, FunctionWrapper,
