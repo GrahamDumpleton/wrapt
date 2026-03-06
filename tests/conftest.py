91f6bab10b6e53b4da5e07e58e8abffa1a9c5898
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-03-06 14:37:54 +1100

    Update conftest.py for newer pytest versions.

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,35 +11,36 @@ class DummyCollector(FileCollector):
     def collect(self):
         return []
 
-def construct_dummy(path, parent):
+def construct_dummy(module_path, parent):
     if hasattr(DummyCollector, "from_parent"):
-        item = DummyCollector.from_parent(parent, fspath=path)
+        item = DummyCollector.from_parent(parent, path=module_path)
         return item
     else:
-        return DummyCollector(path, parent=parent)
+        return DummyCollector(module_path, parent=parent)
 
-def pytest_pycollect_makemodule(path, parent):
-    if '_py33' in path.basename and version < (3, 3):
-        return construct_dummy(path, parent)
-    if '_py34' in path.basename and version < (3, 4):
-        return construct_dummy(path, parent)
-    if '_py35' in path.basename and version < (3, 5):
-        return construct_dummy(path, parent)
-    if '_py36' in path.basename and version < (3, 6):
-        return construct_dummy(path, parent)
-    if '_py37' in path.basename and version < (3, 7):
-        return construct_dummy(path, parent)
-    if '_py38' in path.basename and version < (3, 8):
-        return construct_dummy(path, parent)
-    if '_py39' in path.basename and version < (3, 9):
-        return construct_dummy(path, parent)
-    if '_py310' in path.basename and version < (3, 10):
-        return construct_dummy(path, parent)
-    if '_py311' in path.basename and version < (3, 11):
-        return construct_dummy(path, parent)
-    if '_py312' in path.basename and version < (3, 12):
-        return construct_dummy(path, parent)
-    if '_py3' in path.basename and version < (3, 0):
-        return construct_dummy(path, parent)
-    if '_py2' in path.basename and version >= (3, 0):
-        return construct_dummy(path, parent)
+def pytest_pycollect_makemodule(module_path, parent):
+    name = module_path.name
+    if '_py33' in name and version < (3, 3):
+        return construct_dummy(module_path, parent)
+    if '_py34' in name and version < (3, 4):
+        return construct_dummy(module_path, parent)
+    if '_py35' in name and version < (3, 5):
+        return construct_dummy(module_path, parent)
+    if '_py36' in name and version < (3, 6):
+        return construct_dummy(module_path, parent)
+    if '_py37' in name and version < (3, 7):
+        return construct_dummy(module_path, parent)
+    if '_py38' in name and version < (3, 8):
+        return construct_dummy(module_path, parent)
+    if '_py39' in name and version < (3, 9):
+        return construct_dummy(module_path, parent)
+    if '_py310' in name and version < (3, 10):
+        return construct_dummy(module_path, parent)
+    if '_py311' in name and version < (3, 11):
+        return construct_dummy(module_path, parent)
+    if '_py312' in name and version < (3, 12):
+        return construct_dummy(module_path, parent)
+    if '_py3' in name and version < (3, 0):
+        return construct_dummy(module_path, parent)
+    if '_py2' in name and version >= (3, 0):
+        return construct_dummy(module_path, parent)
