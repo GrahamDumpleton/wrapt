6deda4336713f4b1a6a36c4c157df1662410861d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-06-16 14:14:20 +1000

    Update to 2.2.2rc3.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -13,7 +13,7 @@ def _format_version(parts):
     )
 
 
-__version_info__ = ("2", "2", "2", "rc2")
+__version_info__ = ("2", "2", "2", "rc3")
 __version__ = _format_version(__version_info__)
 
 from .__wrapt__ import (
