5235ff79fac6c60b8e1c0e691e49ce4fd74adab3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-08-06 13:31:09 +1000

    Update for second 2.4.0 dev snapshot.

--- src/wrapt/__init__.py
+++ src/wrapt/__init__.py
@@ -13,7 +13,7 @@ def _format_version(parts):
     )
 
 
-__version_info__ = ("2", "4", "0", "dev1")
+__version_info__ = ("2", "4", "0", "dev2")
 __version__ = _format_version(__version_info__)
 
 from .__wrapt__ import (
